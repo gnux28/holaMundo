8f2ad2437c8e11855c25797660c9eeaac930a54d
gnux28 <juamlopez42@gmail.com>
2023-08-09 12:38:47 -0300

    primer commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      Persona persona = new Persona();
+      persona.setNombre("juan");
+      persona.setEdad(30);
+      System.out.println("Nombre : " + persona.getNombre());
+      System.out.println("edad : " + persona.getEdad());
+      Perro perro = new Perro();
+      perro.setNombre("roky");
+      System.out.println("mi perro se llama : " + perro.getNombre());
+      int entero = 1245678 ;
+
+      System.out.println("numero entero : "+ entero );
+
+      double decimal = 3.45;
+      System.out.println("decimal :"+ decimal);
+      
+      char caracter ='a';
+      System.out.println("caracter : "+ caracter);
+      boolean desicion = false ;
+      System.out.println("desicion :"+desicion);
+      Integer numero = null ;
+      System.out.println(numero );
+      
+      String palabra = "hola que tal ?";
+      System.out.println(palabra);
+
+
+
+
+
+    } 
+
+
+}
---
+++ src/Persona.java
@@ -0,0 +1,18 @@
+public class Persona {
+    private String nombre ;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    private int edad;
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
+
+
